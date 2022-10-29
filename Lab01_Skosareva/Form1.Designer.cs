5b9f82c8dbbfe71d7d6a5d59011240004bdc60d8
Fortuna-Miller <116880117+Fortuna-Miller@users.noreply.github.com>
2022-10-29 13:52:45 +0300

    Add files via upload

---
+++ Lab01_Skosareva/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Lab01_Skosareva
+{
+    partial class Autorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.button_OK = new System.Windows.Forms.Button();
+            this.Login = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Location = new System.Drawing.Point(92, 70);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(100, 22);
+            this.textBox_login.TabIndex = 0;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(92, 112);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.PasswordChar = '*';
+            this.textBox_password.Size = new System.Drawing.Size(100, 22);
+            this.textBox_password.TabIndex = 1;
+            // 
+            // button_OK
+            // 
+            this.button_OK.Location = new System.Drawing.Point(92, 156);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(100, 23);
+            this.button_OK.TabIndex = 2;
+            this.button_OK.Text = "OK";
+            this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // Login
+            // 
+            this.Login.AutoSize = true;
+            this.Login.Location = new System.Drawing.Point(12, 70);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(40, 16);
+            this.Login.TabIndex = 3;
+            this.Login.Text = "Login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // Autorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 250);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.button_OK);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_login);
+            this.Name = "Autorization";
+            this.Text = "Autorization";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Label Login;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Lab01_Skosareva/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab01_Skosareva
+{
+    public partial class Autorization : Form
+    {
+    
+        private string login = "Ann";
+        private string pass = "1234";
+        private int numberOfTryings = 3;
+
+        private int correctInput = 0;
+        private int i = 0;
+
+        public Autorization()
+        {
+            InitializeComponent();
+        }
+
+        private void button_OK_Click(object sender, EventArgs e)
+        {
+            if (textBox_login.Text == login && textBox_password.Text == pass && numberOfTryings != i)
+            {
+                MessageBox.Show("Hello, " + login);
+                correctInput = 1;
+            }
+
+            while (i < numberOfTryings)
+            {
+                if (textBox_login.Text == login && textBox_password.Text != pass)
+                {
+                    MessageBox.Show("Uncorrect password");
+                    textBox_password.Clear();
+                }
+
+                else if (textBox_login.Text != login)
+                {
+                    MessageBox.Show("Uncorrect login");
+                    textBox_login.Clear();
+                }
+
+                i++;
+                break;
+            }
+
+            if(i == numberOfTryings && correctInput != 1)
+            {
+                MessageBox.Show("You have used up all 3 attempts");
+            }
+
+            if(correctInput == 1)
+            {
+                i = 0; 
+            }
+        }
+    }
+}
